f2ad9aac26f47b21506d0cf47629ed220b335303
Ed Fildes <eddfildes@hotmail.com>
2026-04-20 13:50:27 +0100

    added envelope to sounds

--- index.js
+++ index.js
@@ -6,48 +6,66 @@ document.getElementById("fundamental").addEventListener("change", (e => {
     rootFreq = e.target.value
 }))
 
+const mapToRange = (input, in_min, in_max, out_min, out_max)=> {
+  return (input - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
 const ratios = new Set()
 
-function mapPitchToColour(pitch){
-    let octave = Math.trunc(pitch)
-    const sign = Math.sign(octave)
-    const cents = pitch - octave
 
-    console.log(pitch, octave, cents)
+function mapPitchToColour(pitch){
+    let octave = Math.floor(pitch)
+    const sign = Math.sign(octave)
+    const cents = Math.abs(Math.trunc(pitch) - pitch)
+    const totalRange = 360 // over 11 octaves?
+
+    // console.log(pitch, octave, cents)
+
+    const mapping = mapToRange(cents, 0, 1, 174, 242)
 
     // // increase in pitch
     if(sign === 1){
-        return `hsl(${cents/4}turn 100% ${50 + ((50/3) * octave)}%)`
+        return `hsl(${mapping} 100% ${50 + ((50/3) * octave)}%)`
     }
     // root octave
     if(sign === 0){
-        return `hsl(${cents/4}turn 100% 50%)`
+        return `hsl(${mapping} 100% 50%)`
     }
 
     // lower pitch
     else if(sign === -1){
         octave = octave * -1
-        return `hsl(${cents/4}turn 100% ${50 - ((50/3) * octave)}%)`
+        return `hsl(${mapping} 100% ${50 - ((50/3) * octave)}%)`
     }
 }
 
-// effects chain
+// effects chain 
 const reverb = new Tone.Reverb(3).toDestination()
 reverb.wet = 0.5
 const comp = new Tone.Compressor(-30, 3).connect(reverb);
 
-
-document.body.onload = createGrid;
+const ampEnvStore = new Map()
 
 function addTone(y, x, box) {
     if(!box.isActive){
-        box.osc = new Tone.Oscillator(rootFreq * y/x, "sine").connect(comp).start();
+        const ampEnv = new Tone.AmplitudeEnvelope({
+            "attack": 0.4,
+            "decay": 0.4,
+            "sustain": 1.0,
+            "release": 1.0
+        }).connect(comp);
+        ampEnvStore.set(`${x},${y}`, ampEnv)
+        box.osc = new Tone.Oscillator(rootFreq * y/x, "sine").connect(ampEnv).start();
         box.isActive = true;
         box.style.border = "black solid 4px";
+        ampEnv.triggerAttack();
     } else {
-        box.osc.stop();
+        // box.osc.stop();
+        const ampEnv = ampEnvStore.get(`${x},${y}`)
+        ampEnv.triggerRelease();
         box.isActive = false;
         box.style.border = "white solid 1px";
+        ampEnvStore.delete(`${x},${y}`)
     }
 }
 
@@ -56,6 +74,8 @@ function createGrid() {
     wrapper.style.display = "grid"
     wrapper.style.gridTemplateColumns = `repeat(${gridSize}, ${boxSize})`
     wrapper.style.gridTemplateRows = `repeat(${gridSize}, ${boxSize})`
+    wrapper.style.columnGap = "5px";
+    wrapper.style.rowGap = "5px";
 
     for (let y = 0; y < gridSize; y++){
         for (let x = 0; x < gridSize; x++){
@@ -88,3 +108,17 @@ function createGrid() {
     const root = document.getElementById("root")
     root.appendChild(wrapper)
 }
+
+
+function createSpectrum() {
+    const spectrumContainer = document.getElementById("spectrum_container");
+    const spectrum = document.createElement("div");
+    spectrum.style.height = `${root.offsetWidth}px`;
+    spectrum.style.width = "100px"
+    spectrumContainer.appendChild(spectrum);
+    spectrum.style.background = `linear-gradient(to top, hsl(174 100% 50%), hsl(242 100% 50%))`;
+}
+
+
+createGrid()
+createSpectrum()
